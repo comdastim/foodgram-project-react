c60b01db792ccd2880d3ca4e38ca7cfb8570b74f
Дарья Тимохина <c0mdas@yandex.ru>
2022-11-03 02:12:03 +0300

    создание моделей, настройка админки

--- backend/recipes/admin.py
+++ backend/recipes/admin.py
@@ -1,3 +1,42 @@
 from django.contrib import admin
 
-# Register your models here.
+from django.contrib import admin
+
+from .models import Favorite, Ingredient, Recipe, Shopping_cart, Tag
+
+
+
+class TagAdmin(admin.ModelAdmin):
+    list_display = ('name', 'color', 'slug') 
+    search_fields = ('name', 'color', 'slug') 
+    list_filter = ('name', 'color', 'slug',) 
+    empty_value_display = '-пусто-'  
+
+class IngredientAdmin(admin.ModelAdmin):
+    list_display = ('name', 'measurement_unit') 
+    search_fields = ('name') 
+    list_filter = ('name', ) 
+    empty_value_display = '-пусто-'  
+
+class RecipeAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'author') 
+    search_fields = ('author','tag','ingredient') 
+    list_filter = ('name',) 
+    empty_value_display = '-пусто-'  
+
+class FavoriteAdmin(admin.ModelAdmin):
+    list_display = ('id', 'user', 'recipe') 
+    list_filter = ('user',) 
+    empty_value_display = '-пусто-'  
+
+class Shopping_cartAdmin(admin.ModelAdmin):
+    list_display = ('id', 'user', 'recipe') 
+    list_filter = ('user',) 
+    empty_value_display = '-пусто-'  
+
+    
+admin.site.register(Favorite) 
+admin.site.register(Ingredient) 
+admin.site.register(Recipe) 
+admin.site.register(Shopping_cart) 
+admin.site.register(Tag) 
---
+++ backend/recipes/models.py
@@ -0,0 +1,106 @@
+from django.contrib.auth import get_user_model
+from django.db import models
+
+User = get_user_model()
+
+CHOICES = (
+        ('Gray', 'Серый'),
+        ('Black', 'Чёрный'),
+        ('White', 'Белый'),
+        ('Ginger', 'Рыжий'),
+        ('Mixed', 'Смешанный'),
+        ('Yellow', 'Желтый'),
+        ('Red', 'Красный'),
+        ('Green', 'Зеленый'),
+        ('Blue', 'Голубой'),
+        ('Navy blue', 'Синий'),
+        ('Violet', 'Фиолетовый')
+    )
+
+
+
+class Tag(models.Model):
+    name = models.CharField('Тэг', max_length=200, unique=True)
+    color = models.CharField('Цветовой HEX-код', max_length=20, unique=True, choices=CHOICES)
+    slug = models.SlugField(max_length=200, unique=True)
+
+    class Meta:
+        verbose_name = 'Тэг'
+        verbose_name_plural = 'Тэги'
+
+    def __str__(self):
+        return self.name
+    
+
+
+class Ingredient(models.Model):
+    name = models.CharField('Название',max_length=200)
+    measurement_unit = models.CharField('Единицы измерения', max_length=200)
+    
+    class Meta:
+        verbose_name = 'Ингридиент'
+        verbose_name_plural = 'Ингридиенты'
+
+    def __str__(self):
+        return self.name
+
+
+class Recipe(models.Model):
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='recipes') 
+    name = models.CharField('Название рецепта', max_length=200)
+    image = models.ImageField('Картинка',
+        upload_to='recipes/',
+        blank=True
+    )
+    text= models.TextField('Текст рецепта',
+                            help_text='Введите текст рецепта')    
+    ingredients = models.ManyToManyField('Ingredient', through = 'RecipeIngredient')
+    tags = models.ManyToManyField('Tag', through = 'RecipeTag')
+    cooking_time = models.TimeField('Время приготовления')
+
+
+    class Meta:
+        ordering = ['-id']
+        verbose_name = 'Рецепт'
+        verbose_name_plural = 'Рецепты'
+
+    def __str__(self):
+        return self.name
+
+
+class RecipeIngredient(models.Model):
+    recipe = models.ForeignKey(Recipe, on_delete=models.CASCADE)
+    ingredient = models.ForeignKey(Ingredient, on_delete=models.CASCADE)
+    amount = models.IntegerField ('Количество')
+
+    class Meta:
+        verbose_name = 'Количество ингридиента'
+        verbose_name_plural = 'Количество ингридиентов'
+
+    def __str__(self):
+        return f'{self.recipe}{self.ingredient}'
+
+
+class RecipeTag(models.Model):
+    recipe = models.ForeignKey(Recipe, on_delete=models.CASCADE)
+    tag = models.ForeignKey(Tag, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f'{self.recipe}{self.tag}'
+
+
+class Favorite(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name = 'favorite')
+    recipe = models.ManyToManyField('Recipe', related_name = 'favorite')
+
+    class Meta:
+        verbose_name = 'Понравившийся рецепт'
+        verbose_name_plural = 'Понравившиеся рецепты'
+
+
+class Shopping_cart(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name = 'shopping_cart')
+    recipe = models.ManyToManyField('Recipe', related_name = 'shopping_cart')
+
+    class Meta:
+        verbose_name = 'Список покупок'
