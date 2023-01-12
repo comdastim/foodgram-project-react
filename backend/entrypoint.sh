92f83f0070fce271902d42bec168fdc15b92a1c1
Дарья Тимохина <c0mdas@yandex.ru>
2023-01-13 02:29:12 +0300

    dockerignore

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -1,5 +1,6 @@
 # usr/bin/env bash
 python manage.py collectstatic --noinput
 python manage.py migrate
+python manage.py load_ingredients_data
 gunicorn foodgram.wsgi:application --bind 0:8000
 # python manage.py runserver 0.0.0.0:8000
